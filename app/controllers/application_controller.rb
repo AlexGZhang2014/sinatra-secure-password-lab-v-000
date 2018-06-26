b04279a02e03690bf4c66e487b9260efc5781e1c
Alex Zhang <alexgzhang92@gmail.com>
2018-06-26 01:55:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,11 +47,11 @@ class ApplicationController < Sinatra::Base
     @old_balance = @user.balance.to_f
     @withdraw = params[:withdraw].to_f
     if @withdraw <= @old_balance
-      @status = "successful"
+      @status = true
       @user.update(balance: @old_balance - @withdraw)
       redirect '/account'
     else
-      @status = "unsuccessful"
+      @status = false
       redirect '/account'
     end
   end
